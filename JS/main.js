d8da6c34d0aecc30683f71a1c16b7042744a4b70
kmilek <kmilek73@gmail.com>
2022-12-05 19:12:12 +0100

    rozwiązanie

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+let num1 = 2;
+let num2 = 5;
+
+function porownanie(num1,num2){
+    if (num1 < num2){
+        console.log(" wieksza jest zmienna: num2 ", num2 );
+    }else if (num1 > num2) {   
+        console.log(" większa jest zmienna num1: ", num1 );
+    } else { 
+            console.log("zmienne są równe" );
+        }
+}
+
+porownanie(num1,num2);
+
